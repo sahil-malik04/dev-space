c6a77c526ab8a56e25ff01fb643508ae2d58a369
sm <sahilmalik15.sm@gmail.com>
2026-02-04 00:36:35 +0530

    cv update

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -40,7 +40,7 @@ const HeroSection = () => {
               Let’s Talk
             </Link>
             <Link
-              href="/cv/Sahil_Malik_CV.pdf"
+              href="/cv/SAHIL_MALIK_CV.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
